5340b3309a406a3a4b3774c51d54341038efca5f
Volodymyr <vladikon70@gmail.com>
2023-11-18 15:12:50 +0100

    add dotenv

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
-      console.log("Server running. Use our API on port: 3000");
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
