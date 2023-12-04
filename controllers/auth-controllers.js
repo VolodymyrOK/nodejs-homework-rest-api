71a5f0e9d700141df7b47a808709ca3a4a0a9dc5
Volodymyr <vladikon70@gmail.com>
2023-12-04 19:37:43 +0100

    Update auth-controllers.js

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -84,26 +84,28 @@ const logout = async (req, res) => {
 };
 
 const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
+  try {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
 
-  Jimp.read(resultUpload, (err, avatarFile) => {
-    if (err) {
-      throw HttpError(404, `File ${filename} not found`);
-    }
-    avatarFile.resize(250, 250).write(resultUpload);
-  });
+    Jimp.read(resultUpload, (err, avatarFile) => {
+      if (err) {
+        throw HttpError(404, `File ${filename} not found`);
+      }
+      avatarFile.resize(250, 250).write(resultUpload);
+    });
 
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.json({
-    ResponseBody: {
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({
       avatarURL,
-    },
-  });
+    });
+  } catch (error) {
+    throw HttpError(404, `Avatar file not found`);
+  }
 };
 
 module.exports = {
